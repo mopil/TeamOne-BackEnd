c942158c501d7ccd31faa9a502c8f4b3d22f3d65
sectionr0 <sectionr0@gmail.com>
2022-03-30 21:22:14 +0900

    initial commit

---
+++ src/main/java/com/mjuteam2/TeamOne/TeamOneApplication.java
@@ -0,0 +1,13 @@
+package com.mjuteam2.TeamOne;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TeamOneApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TeamOneApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mjuteam2/TeamOne/TeamOneApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mjuteam2.TeamOne;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TeamOneApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
