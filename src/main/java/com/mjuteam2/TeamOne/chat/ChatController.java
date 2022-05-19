3fedf701ebbb3838d8316744f90255434013afbd
sectionr0 <sectionr0@gmail.com>
2022-05-19 21:26:55 +0900

    채팅 서비스 및 컨트롤러
    간단한 테스트 용도로 제작

---
+++ src/main/java/com/mjuteam2/TeamOne/chat/ChatController.java
@@ -0,0 +1,25 @@
+package com.mjuteam2.TeamOne.chat;
+
+import com.mjuteam2.TeamOne.chat.domain.ChatRoom;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequiredArgsConstructor
+@RequestMapping("/chat")
+public class ChatController {
+
+    private final ChatService chatService;
+
+    @PostMapping
+    public ChatRoom createRoom(@RequestParam String name) {
+        return chatService.createRoom(name);
+    }
+
+    @GetMapping
+    public List<ChatRoom> findAllRoom() {
+        return chatService.findAllRoom();
+    }
+}
